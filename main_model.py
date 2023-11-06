606203379c6820b14fab6d46a80e4f5bdea8cff9
Y-Srivaishnavi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2023-11-06 23:38:44 +0530

    Update main_model.py
    Change model from cross-validation based to random NAS

--- main_model.py
+++ main_model.py
@@ -1,47 +1,46 @@
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras.utils import to_categorical
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy
+import randomvariables 
+import randomgeneratesequence
+import randomgeneratemodel 
+import randomsearchspace 
 
-# Define a function to evaluate a given neural network architecture
-def evaluate(architecture):
-    # Create a new Keras model instance using the given architecture
-    model = Sequential()
-    model.add(Dense(units=architecture['hidden_layer_sizes'][0], input_shape=(input_dim,)))
-    model.add(Dense(units=architecture['hidden_layer_sizes'][1], activation='relu'))
-    model.add(Dense(units=architecture['hidden_layer_sizes'][2], activation='relu'))
-    model.add(Dense(units=output_dim, activation='softmax'))
-    
-    # Compile the model with a suitable optimizer and loss function
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
-    
-    # Split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    # Train the model on the training data
-    model.fit(X_train, to_categorical(y_train), epochs=50, batch_size=32, validation_data=(X_test, to_categorical(y_test)))
-    
-    # Evaluate the model on the test data
-    test_loss, test_acc = model.evaluate(X_test, to_categorical(y_test))
-    
-    # Return the evaluation metric (in this case, accuracy)
-    return test_acc
+#list to store data for each iteration
+data =[]
+return_data =[]
 
-# Define a list of possible neural network architectures
-architectures = [
-    {'hidden_layer_sizes': [20, 20, 20]},
-    {'hidden_layer_sizes': [20, 30, 20]},
-    {'hidden_layer_sizes': [30, 20, 20]},
-    {'hidden_layer_sizes': [30, 30, 20]},
-    {'hidden_layer_sizes': [30, 30, 30]}
-]
+#list to store all sequences to check later if any sequences were generated more than once
+samples = []
 
-# Perform neural architecture search using a random search strategy
-random_search = RandomizedSearchCV(estimator=evaluate, param_grid=architectures, n_iter=10, cv=5, scoring='accuracy')
-random_search.fit(X, y)
+#intialize the search space
+vocab = vocab_dict()
 
-# Print the best neural network architecture found by the search
-print('Best architecture:', random_search.best_params_)
+#location to log data
+
+def randomsearch(x_data,y_data,x_val,y_val):
+    #generate the sequence and evalaute and log it
+
+    #iterate over the number of samples we want to generate randomly and evaluate
+    for i in range(numsamples):
+
+        #generate sequence
+        sequence = randomsequencegenerate(vocab,samples)
+        #generate model
+        if sequence != "NA":
+            #print(np.shape(x_data[0]))
+            model = randommodelgenerate(sequence,vocab, np.shape(x_data[0]))
+            #train the model
+            history = trainmodel(model,x_data,y_data,x_val,y_val)
+            #store the val accuracy for the sequence
+            valacc = valmodel(history)
+            #append the results to data and sample
+            samples.append(sequence)
+            data.append((sequence,valacc))
+            #print the data
+            #print(data[i][0])
+            print("Architecture is ",encode_sequences(data[i][0],vocab))
+            print("Validation Accuracy is ", data[i][1])
+            return_data.append([encode_sequences(data[i][0],vocab),data[i][1]])
+        else:
+            data.append("NA")
+            print("Sequence repeated")
+     
+    return return_data
