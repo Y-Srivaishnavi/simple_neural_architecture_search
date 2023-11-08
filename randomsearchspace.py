9460fb7d4bb2062028e8390cf1d55acedb2d37d0
Y-Srivaishnavi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2023-11-08 13:16:55 +0530

    Update search space to accommodate multiple layers

--- randomsearchspace.py
+++ randomsearchspace.py
@@ -1,6 +1,6 @@
 def vocab_dict() -> list:
     """
-    Generates search space of hidden layers and activation function combinations.
+    Creates a dictionary of possible layers for a neural networks, using pre-defined kernel and filter sizes.
     
     Returns
     -------
@@ -16,7 +16,7 @@ def vocab_dict() -> list:
     # kernel_size = [3,5,9,11,13,15,17,19,21,23,25,27,29,31]
     # nodes = [4,8,10,12,16,19,25,50,150,200,300,500,1]
 
-    nodes = [4,8]
+    nodes = [(200,60), (300, 70)]
     activations = ['linear','relu']
 
     # Create a dictionary out of IDs and parameters
