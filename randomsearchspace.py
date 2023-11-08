3338bb957f6532091fcb13b24cf6a94926fd77f2
Y-Srivaishnavi <127314796+Y-Srivaishnavi@users.noreply.github.com>
2023-11-08 12:09:56 +0530

    Change search space to suit classification model

--- randomsearchspace.py
+++ randomsearchspace.py
@@ -1,59 +1,31 @@
-from randomvariables import target_classes
-
-def vocab_dict():
+def vocab_dict() -> list:
     """
-    Creates a dictionary of possible layers for a neural networks, using pre-defined kernel and filter sizes.
+    Generates search space of hidden layers and activation function combinations.
     
     Returns
     -------
-    dict
-        A search-space dictionary containing unique combinations of (filter size, kernel size, number of nodes, activation function)
+    list
+        A search-space list containing unique combinations of (number of nodes, activation function)
     
     Notes
     -----
-    For simplicity, activation function is always relu, strides and padding are one, and dropout size is always 0.5
+    For simplicity, dropout size is always 0.5
     """
-    # Define search space of parameters
-    filter_size = [3,5,9,11,13,15,17,19,21,23,25,27,29,31]
-    kernel_size = [3,5,9,11,13,15,17,19,21,23,25,27,29,31]
-    nodes = [4,8,10,12,16,19,25,50,150,200,300,500,1]
-    activations = ['relu', 'sigmoid', 'softmax']
+    # Define search space of  for image data, taking activation function = "relu"
+    # filter_size = [3,5,9,11,13,15,17,19,21,23,25,27,29,31]
+    # kernel_size = [3,5,9,11,13,15,17,19,21,23,25,27,29,31]
+    # nodes = [4,8,10,12,16,19,25,50,150,200,300,500,1]
+
+    nodes = [4,8]
+    activations = ['linear','relu']
 
     # Create a dictionary out of IDs and parameters
-    vocab = {}
-    id = 100
+    vocab = []
     
-    # Create Cartesian product of all filter and kernel sizes, and node combinations, and assign UID to each tuple
-    for filter in filter_size:
-        for kernel in kernel_size:
-            for node in nodes:
-                id += 1
-                if node != 1:
-                    activation = activations[0]
-                else:
-                    activation = activations[1] if target_classes == 2 else activations[2]
-                      
-                vocab[id] = (filter, kernel, node, activation)
+    # Create Cartesian product of all node combinations, and assign UID to each tuple
+    for node in nodes:
+        for activation in activations:                  
+            vocab.append((node, activation))
     
     return vocab
 
-def encode_sequences(sequence, vocab):
-    """
-    Function to encode an architecture sequence.
-    """
-    values = list(vocab.values())
-    encoded_sequences = []
-    for value in sequence:
-        encoded_sequences.append(values[value-1])
-    return encoded_sequences
-
-def decode_sequence(sequence, vocab):
-    """
-    Function to decode an architecture sequence.
-    """
-    keys=list(vocab.keys())
-    values = list(vocab.values())
-    decoded_sequence = []
-    for key in sequence:
-        decoded_sequence.append(values[keys.index(key)])
-    return decoded_sequence
